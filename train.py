62302be2ae2631a1bb7f4cad1f2f7f7940ec8172
rajkumar1447 <rajkumarbetham7@gmail.com>
2026-03-23 13:08:14 +0530

    train.py updated

---
+++ train.py
@@ -0,0 +1,45 @@
+# Import Necessary Libraries
+import os
+from glob import glob
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from dataset import tf_dataset
+from model import unet3plus
+from metrics import bce_dice_loss, dice_coef, iou
+
+# Function to create Dir
+def create_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+# Global
+BATCH = 4
+EPOCHS = 80
+
+image_paths = sorted(glob("data/images/*"))
+mask_paths = sorted(glob("data/masks/*"))
+
+train_x, val_x, train_y, val_y = train_test_split(
+    image_paths, mask_paths, test_size=0.2, random_state=42
+)
+
+train_ds = tf_dataset(train_x, train_y, BATCH)
+val_ds = tf_dataset(val_x, val_y, BATCH)
+
+model = unet3plus((512,512,3))
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(1e-4),
+    loss=bce_dice_loss,
+    metrics=[dice_coef, iou]
+)
+
+create_dir("files")
+
+callbacks = [
+    tf.keras.callbacks.ModelCheckpoint("files/model.h5", save_best_only=True),
+    tf.keras.callbacks.ReduceLROnPlateau(patience=5),
+    tf.keras.callbacks.EarlyStopping(patience=10)
+]
+
+model.fit(train_ds, validation_data=val_ds, epochs=EPOCHS, callbacks=callbacks)
