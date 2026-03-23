e46f74c0a7a2a8bdc1b59b06510ed520ed6c3567
rajkumar1447 <rajkumarbetham7@gmail.com>
2026-03-23 12:57:18 +0530

    metrics.py updated

--- metrics.py
+++ metrics.py
@@ -1,5 +1,4 @@
 #  Import Necessary Libraries
-from networkx import intersection
 import tensorflow as tf
 
 smooth = 1e-6
@@ -8,6 +7,7 @@ smooth = 1e-6
 def dice_coef(y_true, y_pred):
     y_true = tf.keras.layers.Flatten()(y_true)
     y_pred = tf.keras.layers.Flatten()(y_pred)
+    intersection = tf.reduce_sum(y_true * y_pred)
     return (2. * intersection + smooth) / (
         tf.reduce_sum(y_true) + tf.reduce_sum(y_pred) + smooth
     )
