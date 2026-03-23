6b9affe34d4a6d2b1a68c478b670f8be13c09f98
rajkumar1447 <rajkumarbetham7@gmail.com>
2026-03-23 13:11:13 +0530

    gradcam.py updated

---
+++ gradcam.py
@@ -0,0 +1,25 @@
+# Import Necessary Libraries.
+import tensorflow as tf
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+# Function for the gradcam.
+def gradcam(model, img):
+    grad_model = tf.keras.models.Model(
+        [model.inputs],
+        [model.layers[-3].output, model.output]
+    )
+
+    with tf.GradientTape() as tape:
+        conv_out, pred = grad_model(img)
+        loss = tf.reduce_mean(pred)
+
+    grads = tape.gradient(loss, conv_out)
+    weights = tf.reduce_mean(grads, axis=(0,1,2))
+    cam = np.dot(conv_out[0], weights)
+
+    cam = np.maximum(cam,0)
+    cam = cam / cam.max()
+
+    return cam
